f4e1e08069c1343861cee6c5570bcef4d0f580f4
Md zahid hasan tanim(2233081181) <150315114+tanimit@users.noreply.github.com>
2023-11-21 10:38:55 -0800

    Add files via upload

---
+++ odd&even number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    for(a=1;a>=7; a++)
+    {
+        for(b=0;b<=a; b++)
+        {
+            printf("*");
+        }
+            printf("\n");
+    }
+
+}
